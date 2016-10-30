2182e767752f2b3a8d3dfdda04bf961a5b8381d3
ishigo <145uoka@gmail.com>
2016-10-31 03:16:47 +0900

    1st commit

---
+++ app/Http/Controllers/Api/line/LineCallbackController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api\line;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+// use Illuminate\Http\Request;
+use Request;
+
+class LineCallbackController extends Controller {
+    /**
+     * 新しいコントローラーインスタンスの生成
+     *
+     * @return void
+     */
+    public function __construct() {
+        // $this->middleware('auth');
+    }
+    
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        $request = Request::instance(); // Access the instance
+        echo $request->getContent();
+        file_put_contents ( "php://stdout", $request->getContent() );
+        return;
+    }
+}
